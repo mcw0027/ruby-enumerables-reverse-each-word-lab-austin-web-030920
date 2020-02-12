858f567706db2a9970598d02ad8269d56a388ef1
Michael Wixson <wixsonmc@gmail.com>
2020-02-12 22:07:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(sentence)
   base = sentence.split(",")
+  new = []
   i = 0
   while i < base.length do
-    base[i].reverse
+    new.push(base[i].reverse)
+    i += 1
   end
+  return new.to_s
 end
