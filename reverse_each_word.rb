7ea406cb1be06021919817cf023e892eb5e167ce
Michael Wixson <wixsonmc@gmail.com>
2020-02-12 22:09:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,3 @@
 def reverse_each_word(sentence)
-  base = sentence.split(",")
-  new = []
-  i = 0
-  while i < base.length do
-    new.push(base[i].reverse)
-    i += 1
-  end
-  return new.to_s
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
